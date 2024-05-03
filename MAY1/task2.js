b48cbe4269d984caed40e3200deb32781fde52c4
PraveenKothagolla <kothagollolu999@gmail.com>
2024-05-03 09:38:55 +0530

    d

--- MAY1/task2.js
+++
@@ -1,8 +0,0 @@
-currentdate=new Date();
-var currentYear=currentdate.getFullYear();
-var age=(date)=>{
-   birthDate=new Date(date);
-   birthYear=birthDate.getFullYear();
-   console.log(currentYear-birthYear);
-}
-age('2000-07-03');
