de48a9202b11bf36cb509379b7cec18925246766
PraveenKothagolla <158676548+PraveenKothagolla@users.noreply.github.com>
2024-05-03 07:37:05 +0530

    Update every.js

--- MAY2/every.js
+++ MAY2/every.js
@@ -1,4 +1,4 @@
-// It is also same some method but here it will check every element from array for the condition to be 
+// It is also same as some method but here it will check every element from array for the condition to be 
 // satisfied.
 let arr=[10,20,30,40,50]
 let result=arr.every(n=>{
